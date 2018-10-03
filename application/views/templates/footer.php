10bb916b2ab4d6b5635fb3298afee1e58e9abcbf
Matleyx78 <matmat78@hotmail.it>
2018-10-03 15:40:27 +0200

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,10 @@
 </div>     
-<div id="footer" style="background-color: #1e30a5; vertical-align: middle;">
+<div id="footer" class="footblock">
   <div class="container">
     <p class="text-muted navbar-text"><em>&copy; 2018 www.chiuna.it - <?php echo CI_VERSION;?></em></p>
   </div>
 </div>
 
-<script src="<?php echo base_url(); ?>style/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/bootstraporiginal/js/bootstrap.js" defer></script>
 <script src="<?php echo base_url(); ?>assets/js-personal/bootstrap-submenu.js" defer></script>
 <script src="<?php echo base_url(); ?>assets/js-personal/docs.js" defer></script>
