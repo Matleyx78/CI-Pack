d7cef06efac9512a6646392f602eaff43d918c39
Matleyx78 <matmat78@hotmail.it>
2018-10-11 14:56:57 +0200

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="<?php echo base_url(); ?>img/ci-icon.png" />
         <title>My Codeigniter</title>
                 
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstraporiginal/css/bootstrap.min.css">
