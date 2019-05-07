592debc49325706b7eb547de9780d1c04606916a
Matleyx78 <matmat78@hotmail.it>
2019-05-07 10:26:24 +0200

    Update Crud.php

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -326,7 +326,7 @@ $controller .= '
 $controller .= '
 
            $this->load->library(\'form_validation\');
-           
+           $this->form_validation->set_error_delimiters(\'<div class="alert alert-danger">\', \'</div>\');
 ';
            
                 foreach ($fields as $field) {
@@ -625,6 +625,7 @@ $view .= '
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <label for="'.$field_name.'" class="control-label">'.$label.'</label>
                             <div class="form-group">
+			    	<?php echo form_error('.$field_name.'); ?>
                                     <input type="text" name="'.$field_name.'" value="<?php echo $this->input->post(\''.$field_name.'\'); ?>" class="form-control" id="'.$field_name.'" />
                             </div>
                         </div>                            
